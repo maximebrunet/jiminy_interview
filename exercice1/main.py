919a7a0dda6c94bfe8a0e04bcc01f9eabed04453
guillaumeb <guillaume.burger@wandercraft.eu>
2022-05-17 11:13:23 +0200

    exercice 1

---
+++ exercice1/main.py
@@ -0,0 +1,18 @@
+import numpy as np
+from jiminy_py.simulator import Simulator
+from jiminy_py.robot import BaseJiminyRobot
+
+SIMULATION_END_TIME = 10.0
+urdf_path = "double_pendulum.urdf"
+hardware_path = "double_pendulum_hardware.toml"
+
+robot = BaseJiminyRobot()
+robot.initialize(urdf_path, hardware_path, has_freeflyer=False)
+
+simulator = Simulator(robot)
+simulator.import_options("simulation_config.toml")
+
+q0 = np.zeros(2)
+v0 = np.random.rand(2)
+simulator.simulate(SIMULATION_END_TIME, q0, v0)
+simulator.replay()
